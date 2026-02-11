3bee7f639fbc1700c6d435222ace0bbed1ce70a5
Ann Mary Walton <155218373+annwalton@users.noreply.github.com>
2026-02-11 20:09:28 +0530

    Add files via upload

---
+++ inher.py
@@ -0,0 +1,32 @@
+class Python_adv:
+    def course():
+        print("Advanced Python")
+
+
+class Group1(Python_adv):
+    def students():
+        print("Group 1 students")
+
+
+class Group2(Python_adv):
+    def students():
+        print("Group 2 students")
+
+
+class Group3(Python_adv):
+    def students():
+        print("Group 3 students")
+
+
+class Group4(Python_adv):
+    def students():
+        print("Group 4 students")
+
+
+Python_adv.course()
+Group1.students()
+Group2.students()
+Group3.students()
+Group4.students()
+
+    
